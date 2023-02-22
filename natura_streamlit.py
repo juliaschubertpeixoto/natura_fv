a5bb04ec8e83ee86ac00cb1a4349e2014c0a64ff
Júlia Schubert Peixoto <juliaschubertpeixoto@gmail.com>
2023-02-22 21:59:15 +0000

    test button

--- natura_streamlit.py
+++ natura_streamlit.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title("Projeto Berçário")
+
+st.file_uploader("Selecione um arquivo", type='csv')
+
+calculate = st.button("Calcular métricas")
+if calculate:
+    st.write("funcionou")
+else:
+    st.write('não clicou')
