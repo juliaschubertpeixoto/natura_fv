9fe2692b7d014d2338ea950279c8066ad7f296a2
Júlia Schubert Peixoto <juliaschubertpeixoto@gmail.com>
2023-02-23 22:20:01 +0000

    html formatting (3)

--- natura_streamlit.py
+++ natura_streamlit.py
@@ -64,21 +64,21 @@ if up_file:
         col1, col2, col3 = st.columns(3)
         with col1:
             st.metric("Bronze", f"{bronze:.1f}%")
-            st.markdown('''
-                <style>
-                /*center metric label*/
-                [data-testid="stMetricLabel"] > div:nth-child(1) {
-                    justify-content: center;
-                }
-
-                /*center metric value*/
-                [data-testid="stMetricValue"] > div:nth-child(1) {
-                    justify-content: center;
-                }
-                </style>
-                ''', unsafe_allow_html=True)
             st.metric("Ativas", f"{ativa:.1f}%")
             st.metric("Débito", f"{debito:.1f}%")
+        st.markdown('''
+            <style>
+            /*center metric label*/
+            [data-testid="stMetricLabel"] > div:nth-child(1) {
+                justify-content: center;
+            }
+
+            /*center metric value*/
+            [data-testid="stMetricValue"] > div:nth-child(1) {
+                justify-content: center;
+            }
+            </style>
+            ''', unsafe_allow_html=True)
         with col2:
             st.metric("Prata", f"{prata:.1f}%")
             st.metric("Digitalização", f"{digital:.1f}%")
