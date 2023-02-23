7f2157431f237bb982691365d07359de19cd7675
Júlia Schubert Peixoto <juliaschubertpeixoto@gmail.com>
2023-02-23 17:05:31 +0000

    update and

--- natura_streamlit.py
+++ natura_streamlit.py
@@ -21,7 +21,7 @@ if up_file:
         st.success('Métricas calculadas com sucesso!', icon="✅")
         col1, col2, col3 = st.columns(3)
         with col1:
-            if setor & grupo:
+            if ((setor is not None) & (grupo is not None)):
                 bronze = df[(df['Setor']==setor) & (df['Grupo']==grupo) & (df['Nível']=='BRONZE')].count()/df[(df['Setor']==setor) & (df['Grupo']==grupo)].count()
                 st.metric("Bronze", bronze)
         with col2:
